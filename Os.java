3cc61fbdb435ae8ada62838896aebd4d810af8a3
Your Name <you@example.com>
2022-12-26 21:19:01 +0200

    add files

---
+++ Os.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package os;
+
+/**
+ *
+ * @author sharif
+ */
+public class Os {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("welcom");
+        // TODO code application logic here
+    }
+    
+}
